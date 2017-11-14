1a6ba40b567b2ded4f7e493a61a2e2a3ce5ac3b7
Alex Step <alex.step.kellas@gmail.com>
2017-11-14 15:21:34 +0200

    add signHash warning

--- src/Eth/Account.js
+++ src/Eth/Account.js
@@ -236,7 +236,14 @@ export default class Account {
 	 * @memberOf {Account}
 	 */
 	signHash(hash){
-		return signHash(hash, this.exportPrivateKey() )
+		hash = Utils.add0x(hash)
+		if (this.web3.utils.isHexStrict(hash)) {
+			console.log(hash+' is not correct hex')
+			console.log('Use DCLib.Utils.makeSeed or Utils.soliditySHA3(your_args) to create valid hash')
+			throw new Error('invalid hash')
+		}
+
+		return signHash(hash, Utils.add0x(this.exportPrivateKey()) )
 	}
 
 
