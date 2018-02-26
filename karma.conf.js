44a98d0091f00d74b969ec23bec10831951a5e61
jarick <zolotarev.jar@gmail.com>
2018-02-26 18:52:18 +0200

    fix webpack compilation in karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      // { pattern: 'src/index.js', included: true, served: true, watched: false },
-      { pattern: 'dist/DC.js', included: true, served: true, watched: false },
+      { pattern: 'src/index.js', included: true, served: true, watched: false },
       'test/*'
     ],
 
