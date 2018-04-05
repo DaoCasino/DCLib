9a5a9623caa009f78089a61f7a6dbe9582c2216c
Ilya Shvyrialkin <ilyashvy@gmail.com>
2018-04-05 14:17:46 +0300

    change signal server

--- src/config/ropsten/config.js
+++ src/config/ropsten/config.js
@@ -19,8 +19,9 @@ module.exports = {
   network: 'ropsten',
   rpc_url: 'https://ropsten.infura.io/JCnK5ifEPH9qcQkX0Ahl',
   api_url: 'https://platform.dao.casino/faucet2',
-  signal  : '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
-  // api_url: 'https://platform.dao.casino/faucet2?get=accounts',
+  // signal  : '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+  // signal  : '/ip4/46.101.244.101/tcp/9090/ws/p2p-websocket-star/',
+  signal  : '/ip4/146.185.173.84/tcp/9090/ws/p2p-websocket-star/',
 
   contracts: {
     erc20: erc20,
