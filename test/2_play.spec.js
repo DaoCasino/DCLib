ea1258dace87f830e505bea211160bc6bff0f7d3
Alex Step <alex.step.kellas@gmail.com>
2018-04-25 16:55:15 +0300

    update dice logic in test

--- test/2_play.spec.js
+++ test/2_play.spec.js
@@ -20,42 +20,42 @@ const initDCLibAndDApp = function (callback) {
     let history = []
 
     var Roll = function (user_bet, user_num, random_hash) {
-     // convert 1BET to 100000000
-     user_bet = DCLib.Utils.bet2dec(user_bet)
+      // convert 1BET to 100000000
+      user_bet = DCLib.Utils.bet2dec(user_bet)
 
-     // generate random number
-     const random_num = DCLib.numFromHash(random_hash, 0, MAX_RAND_NUM)
+      // generate random number
+      const random_num = DCLib.numFromHash(random_hash, 0, MAX_RAND_NUM)
 
-     let profit = -user_bet
-     // if user win
-     if (user_num >= random_num) {
-       profit = (user_bet * (MAX_RAND_NUM - MAX_RAND_NUM * HOUSEEDGE) / user_num) - user_bet
-     }
+      let profit = -user_bet
+      // if user win
+      if (user_num >= random_num) {
+        profit = (user_bet * (MAX_RAND_NUM - MAX_RAND_NUM * HOUSEEDGE) / user_num) - user_bet
+      }
 
-     // add result to paychannel
-     _self.payChannel.addTX(profit)
-     _self.payChannel.printLog()
+      // add result to paychannel
+      _self.payChannel.addTX(profit)
+      _self.payChannel.printLog()
 
-     // push all data to our log
-     // just for debug
-     const roll_item = {
-       timestamp   : new Date().getTime(),
-       user_bet    : user_bet,
-       profit      : profit,
-       user_num    : user_num,
-       balance     : _self.payChannel.getBalance(),
-       random_hash : random_hash,
-       random_num  : random_num
-     }
-     history.push(roll_item)
+      // push all data to our log
+      // just for debug
+      const roll_item = {
+        timestamp   : new Date().getTime(),
+        user_bet    : user_bet,
+        profit      : profit,
+        user_num    : user_num,
+        balance     : _self.payChannel.getBalance(),
+        random_hash : random_hash,
+        random_num  : random_num
+      }
+      history.push(roll_item)
 
-     return roll_item
+      return roll_item
     }
 
     return {
-     roll    : Roll,
-     history : history
-   }
+      roll    : Roll,
+      history : history
+    }
   })
 
   DCLib.on('ready', async function () {
