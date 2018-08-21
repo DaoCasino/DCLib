8e30f92da895d45ca6357937224deb2653d46b9a
Ilya fanyShu <ilyashvy@gmail.com>
2018-08-21 19:45:13 +0300

    refactor DApps/DApp.js

--- src/DApps/DApp.js
+++ src/DApps/DApp.js
@@ -874,8 +874,7 @@ export default class DApp {
      * @memberOf DApp
      */
 
-  findTheMaxBalance (repeat = 0) {
-    let reduceDeposit  = 0
+  findTheMaxBalance (repeat = 0, reduceDeposit = 0) {
     const checkBalance = data => {
       if (repeat < 10) {
         reduceDeposit   = (reduceDeposit < data.deposit) ? data.deposit : reduceDeposit
